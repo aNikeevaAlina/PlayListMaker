92bc7a57ee3afd8ff3b51e6866e31aba0946d687
Alina <anikeevaaa99@gmail.com>
2023-11-16 20:26:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/playlistmaker/SearchMediaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchMediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_media_activity)
+    }
+}
